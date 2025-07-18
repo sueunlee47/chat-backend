f659e52c71d9b2d9a2a95939e4180c0620d4aeab
sueunlee <sue@playauto.co.kr>
2025-07-18 10:13:14 +0900

    feat: add websocket gateway lifecycle hooks - afterInit, handleConnection, handleDisconndect

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,31 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chat' })
-export class ChatGateway {
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  afterInit(server: Server) {
+    console.log('웹소켓 서버 초기화 완료');
+  }
+  // 클라이언트 웹소켓 서버에 연결했을때 (소켓 연결 시점)
+  handleConnection(client: Socket) {
+    console.log('클라이언트 연결: ', client.id);
+  }
+
+  // 클라이언트 웹소켓 서버에 연결 해제했을때 (소켓 연결 시점)
+  handleDisconnect(client: Socket) {
+    console.log('클라이언트 연결 해제: ', client.id);
+  }
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(client: Socket, payload: any): string {
     return 'Hello world!';
   }
 }
